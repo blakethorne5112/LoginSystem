10c0866fd2b942bef67147b2638170f045d3d054
Jasper3671 <blake.thorne@hotmail.com>
2022-07-22 18:07:43 +1000

    Basic Login System Finalised
    Complete

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int initLogin();
-int Login(bool);
-int Register();
+void Login();
+void Register();
+string user;
+string pass;
 
 int initLogin(){
     int logOrReg = 0;
@@ -12,11 +14,9 @@ int initLogin(){
     cout<< "Please enter your answer here: ";
     cin >> logOrReg;
     if(logOrReg == 1){
-        cout << "Success!" << endl;
         result = 1;
     }
     else if(logOrReg == 2){
-        cout << "Success!" << endl;
         result = 2;
     }
     else{
@@ -26,30 +26,51 @@ int initLogin(){
     return result;
 }
 
-int Login(bool n){
-    if(n == false){
-        cout << "No Accounts exist yet. Please Register an Account first." << endl;
-        return 0;
+void Login(){
+    string tempuser, temppass;
+    cout << "Please input your username." << endl;
+    cin >> tempuser;
+    if(tempuser != user){
+        while(tempuser != user){
+            cout << "This username does not match any of our records. Please re-enter the username." << endl;
+            cin >> tempuser;
+        }
     }
-
+    cout << "Please input your password." << endl;
+    cin >> temppass;
+    if(temppass != pass){
+        while(temppass != pass){
+            cout << "This username and password combination does not match any of our records. Please re-enter the password." << endl;
+            cin >> temppass;
+        }
+    }
+    cout << "Login Successful! Thanks for logging in. End of Program." << endl;
 }
 
-int Register(){
-    cout << "Your entry was not one of the options. Please try again." << endl;
+void Register(){
+    cout << "Please input your username." << endl;
+    cin >> user;
+    cout << "Please input your password." << endl;
+    cin >> pass;
 }
 
 int main(){
     int result;
-    bool AccExists;
+    bool AccExists = false;
     result = initLogin();
     if(result == 1){
-        int result = Login(AccExists);
-        if(result == 0){
+        if(AccExists == false){
+            cout << "No Accounts exist yet. Please Register an Account first." << endl;
             Register();
+            bool AccExists = true;
+            cout << "Thanks for Registering! Now, time to Login." << endl;
         }
     }
     else{
         Register();
+        bool AccExists = true;
+        cout << "Thanks for Registering! Now, time to Login." << endl;
     }
+    Login();
 }
 
